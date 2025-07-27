8083944df2b550c675bcc8344e662c9625d09637
Alex Guryanov <alexandear81@gmail.com>
2025-07-27 18:14:11 +0200

    Trigger deploy

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,7 +3,7 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 export const LoginButton = () => {
   const login = async () => {
-    console.log("🔥 login function triggered!");
+    console.log("🔥 login function triggered! And now it will work :)");
     const provider = new GoogleAuthProvider();
     try {
       const result = await signInWithPopup(auth, provider);
